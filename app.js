83db42c4a76664e6c80b2a9b94b23f9cc029b87a
Mateo <mgomezossa@gmail.com>
2020-04-28 20:35:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,15 @@ const ProductsSchema = new mongoose.Schema({
   });
 const Products = mongoose.model("Products", ProductsSchema);
 
-app.use(express.json());
+// contentType: "application/json"
 
+app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.get("/products", async (req, res) => { //GET / - muestra la lista de usuarios registrados.
     const producto = await Products.find();
        res.send(JSON.stringify(producto));
